804995ace195e4ea74026157312fe83679b8d672
Kshikhar97 <98159808+Kshikhar97@users.noreply.github.com>
2025-08-30 19:31:50 +0530

    Add files via upload

---
+++ Assignment_1_2.py
@@ -0,0 +1,5 @@
+f_name = input("Enter your first name: ")
+l_name = input("Enter your last name: ")
+
+greet = ' Welcome to the Python program.'
+print('Hello, ' + f_name + ' ' + l_name + '!' + greet)
